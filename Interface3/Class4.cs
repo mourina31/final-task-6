35d498c41459268da4a4b846a0c1e5c0ea1c8846
mourina31 <hriditatasnim31@gmail.com>
2020-11-27 20:40:44 +0600

    first commit

---
+++ Interface3/Class4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface3
+{
+    interface RadioPlayerInterface
+    {
+        void switchs(bool on);
+        void retune(double frequency);
+        void setVolume(int loudness);
+        void changeChannel();
+    }
+}
---
+++ Interface3/Class5.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Music ");
+
+            Phone m1 = new Phone("It's the Most Wonderful Time of the Year", "Andy Williams", 2020, 300);
+            m1.ShowInfo();
+            Console.WriteLine();
+
+            m1.changeTitle("It is the Most Wonderful Time of the Year");
+            m1.ShowInfo();
+            Console.WriteLine();
+            Console.WriteLine("Swich On Call");
+            m1.switchs(true);
+            Console.WriteLine();
+            Console.WriteLine("Music Play On Call");
+            m1.play(true);
+            Console.WriteLine();
+            Console.WriteLine("Set Volume Call with Loudness 5");
+            m1.setVolume(5);
+            Phone m2 = new Phone("Jingle Bell Rock", "Bobby Helms", 2020, 300);
+            Phone m3 = new Phone("Holly Jolly Christmas", "Burl Ives", 2020, 300);
+            Phone m4 = new Phone("Memories", "Maroon 5", 2020, 300);
+            Phone list = new Phone();
+            list.AddMusicFile(m2, m1, m3, m4);
+            Console.WriteLine();
+            Console.WriteLine("Next Music Info");
+            list.playNext();
+            Console.WriteLine();
+            Console.WriteLine("Previous Music Info");
+            list.playPrevious();
+            Console.WriteLine();
+            Console.WriteLine("Music List");
+
+
+            list.ShowAllMusicFile();
+
+        }
+    }
+}
